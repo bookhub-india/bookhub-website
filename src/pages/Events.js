a0dd818c14579cea0492e8818d8eb078acacecf8
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-11-30 20:06:25 +0000

    events page new update

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import Slideshow from './Slideshow';
-import image1 from '../assets/image1.jpg';
-import image2 from '../assets/image2.jpg';
-import image3 from '../assets/image3.jpg';
+import image1 from '../assets/Event-1.png';
+import image2 from '../assets/Event-2.png';
+import image3 from '../assets/Event-3.png';
 
 const eventsData = [
     {
-        title: 'Event One',
-        images: [image1, image2],
-        description: 'Description of Event One.'
+        title: 'Inks & Aspirations',
+        images: [image1, image2, image3],
+        description: 'A handcrafted workshop by YouthSphere and Bookhub in collaboration. Our valuable guest speaker Kashish Gaur will be sharing their invaluable knowledge and experience with you. If you are someone, who loves writing and wishes to pursue it as a career, but doesnt know the possible options, this workshop is for you! Inks and Aspirations: Careers in Writing, helps you gain knowledge about all the possible careers in the field of writing. No there are many more options than being an author! Date - November 24, 2024 Time - 5:00 PM IST Duration - 90 minutes Venue - Google Meet Its free obviously. '
     },
     {
         title: 'Event Two',
-        images: [image2, image3],
+        images: ['image4', 'image5'],
         description: 'Description of Event Two.'
     },
     {
         title: 'Event Three',
-        images: [image3],
+        images: ['image6'],
         description: 'Description of Event Three.'
     }
 ];
@@ -47,7 +47,7 @@ const Events = () => {
             <div className="event-section">
                 <h2>{eventsData[currentEventIndex].title}</h2>
                 <Slideshow images={eventsData[currentEventIndex].images} />
-                <p>{eventsData[currentEventIndex].description}</p>
+                <p className="event-description">{eventsData[currentEventIndex].description}</p>
                 <div className="controls">
                     <div className="arrow left" onClick={goToPreviousEvent}>&lt;</div>
                     <div className="arrow right" onClick={goToNextEvent}>&gt;</div>
