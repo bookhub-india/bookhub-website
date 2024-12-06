692b02b1a17ecc18e86f2aa930eaed00ac0860b5
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-12-06 13:10:31 +0000

    navabr alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/BOOKHUB-LOGO.png';
 import '../index.css';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className='navbar'>
             <Link to='/' className='logo'>
-                <img src={logo} alt="Home" style={{ width: '105px', height: '97.22px', position: 'absolute', left: '3.06px', top: '4.56px' }} />
+                <img src={logo} alt="Home" className="logo-image" />
             </Link>
-            <Link to='/about-us' className='button about_us'>ABOUT US</Link>
-            <Link to='/join-us' className='button join_us'>JOIN US</Link>
-            <Link to='/events' className='button events'>EVENTS</Link>
-            <Link to='/contact' className='button contact'>CONTACT</Link>
-            <Link to='/Submission' className='button projects'>Submission</Link>
-            <Link to='/donate' className='button donate'>DONATE</Link>
+            <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                <Link to='/about-us' className='button about_us'>ABOUT US</Link>
+                <Link to='/join-us' className='button join_us'>JOIN US</Link>
+                <Link to='/events' className='button events'>EVENTS</Link>
+                <Link to='/contact' className='button contact'>CONTACT</Link>
+                <Link to='/submission' className='button projects'>SUBMISSION</Link>
+                <Link to='/donate' className='button donate'>DONATE</Link>
+            </div>
+            <div className='hamburger' onClick={toggleMenu}>
+                {/* Hamburger icon */}
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+            </div>
         </div>
     );
 };
