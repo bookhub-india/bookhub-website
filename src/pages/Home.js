9f7b48d302273deedf8db92c700e7bd53debac34
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-12-08 14:31:00 +0000

    addition of social icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import bgImage1 from '../assets/image1.jpg'; // First background image
 import bgImage2 from '../assets/image2.jpg'; // Second background image
 import bgImage3 from '../assets/image3.jpg'; // Third background image
 import './Home.css'; // Ensure you import the CSS file
+import { FaInstagram, FaEnvelope, FaLinkedin } from 'react-icons/fa';
 
 const logos = [logo1, logo2, logo3]; // Array of logos
 const backgrounds = [bgImage1, bgImage2, bgImage3]; // Array of background images
@@ -46,6 +47,17 @@ const Home = () => {
                 <Link to="/about-us" className="home-button about-us">ABOUT US</Link>
                 <Link to="/contact" className="home-button contact-us">CONTACT</Link>
             </div>
+            <div className="social-icons">
+          <a href="mailto:bookhubtindia@gmail.com" target="_blank" rel="noopener noreferrer">
+            <FaEnvelope />
+          </a>
+          <a href="https://instagram.com/bookk_hubbb" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/company/bookhubt/mycompany/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
         </div>
     );
 };
